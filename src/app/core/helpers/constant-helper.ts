890dfb687f76dd148608f41cbae0d01c41010a00
judinilson <judinilson0000@gmail.com>
2021-09-14 21:15:14 +0400

    create cases

--- src/app/core/helpers/constant-helper.ts
+++ src/app/core/helpers/constant-helper.ts
@@ -1,23 +1,47 @@
-import {environment} from "../environments/environment";
+import { environment } from '../environments/environment';
 
 export class ConstantHelper {
+  static readonly urlAuth = `${environment.apiUrl}/auth`;
+  static readonly urlUserAdmin = `${environment.apiUrl}/userAdmin`;
+  static readonly urlUser = `${environment.apiUrl}/user`;
+  static readonly urlNews = `${environment.apiUrl}/news`;
+  static readonly urlUpload = `${environment.apiUrl}/upload`;
 
-  static readonly urlAuth = `${environment.apiUrl}/auth`
-  static readonly urlUserAdmin = `${environment.apiUrl}/userAdmin`
-  static readonly urlUser = `${environment.apiUrl}/user`
-  static readonly urlNews = `${environment.apiUrl}/news`
-  static readonly urlUpload = `${environment.apiUrl}/upload`
+  static readonly close_big = 'assets/images/close.svg';
+  static readonly picture404 = 'assets/images/error-image.png';
+  static readonly pictureLogo = 'assets/images/logo.png';
+  static readonly pictureLogoW = 'assets/images/logo-w.png';
+  static readonly pictureDefaultNews = 'assets/images/news-illust.svg';
+  static readonly picturePlay = 'assets/images/play.svg';
+  static readonly pictureAccountBg = 'assets/images/bg-acc.jpg';
+  static readonly pictureNicheDefault = 'assets/images/house.svg';
+  static readonly plus_icon = 'assets/icons/plus.svg';
 
-  static readonly picture404 = 'assets/images/error-image.png'
-  static readonly pictureLogo = 'assets/images/logo.png'
-  static readonly pictureLogoW = 'assets/images/logo-w.png'
-  static readonly pictureDefaultNews = 'assets/images/news-illust.svg'
-  static readonly picturePlay = 'assets/images/play.svg'
-  static readonly pictureAccountBg = 'assets/images/bg-acc.jpg'
-  static readonly pictureNicheDefault = 'assets/images/house.svg'
+  static readonly usericon = 'assets/images/user_icon.png';
+  static readonly clothes1 = 'assets/images/clothes/1.jpg';
+  static readonly clothes2 = 'assets/images/clothes/2.jpg';
+  static readonly clothes3 = 'assets/images/clothes/3.jpg';
 
-  static readonly USER_TOKEN = "TOKEN"
-  static readonly MINUTE_OFFSET_REFRESH = 4
-  static readonly EMPTY_JSON = "{}"
-  static readonly platformName = "EzTarget"
+  static readonly telegramicon = 'assets/images/social/telegram.png';
+  static readonly vkicon = 'assets/images/social/vk.png';
+  static readonly instagramicon = 'assets/images/social/instagram.png';
+  static readonly tiktokicon = 'assets/images/social/tiktok.png';
+  static readonly youtubeicon = 'assets/images/social/youtube.png';
+  static readonly whatssapicon = 'assets/images/social/whatssap.png';
+  static readonly callicon = 'assets/images/social/callicon.png';
+  static readonly like = 'assets/images/niche/like.svg';
+  static readonly unlike = 'assets/images/niche/unlike.svg';
+  // unactive
+  static readonly telegram = 'assets/icons/telegram.svg';
+  static readonly instagram = 'assets/icons/instagram.svg';
+  static readonly tiktok = 'assets/icons/tiktok.svg';
+  static readonly vk = 'assets/icons/vk.svg';
+  static readonly youtube = 'assets/icons/youtube.svg';
+  static readonly whatssap = 'assets/icons/whatssap.svg';
+  static readonly call = 'assets/icons/call.svg';
+
+  static readonly USER_TOKEN = 'TOKEN';
+  static readonly MINUTE_OFFSET_REFRESH = 4;
+  static readonly EMPTY_JSON = '{}';
+  static readonly platformName = 'EzTarget';
 }
--- src/app/ui/cases/cases-create/cases-create.component.ts
+++ src/app/ui/cases/cases-create/cases-create.component.ts
@@ -1,15 +1,113 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ConstantHelper } from 'src/app/core/helpers/constant-helper';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 @Component({
   selector: 'app-cases-create',
   templateUrl: './cases-create.component.html',
-  styleUrls: ['./cases-create.component.scss']
+  styleUrls: ['./cases-create.component.scss'],
 })
 export class CasesCreateComponent implements OnInit {
-
-  constructor() { }
+  consts = ConstantHelper;
+  CreateCaseForm: any;
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    this.CreateCaseForm = new FormGroup({
+      name: new FormControl(''),
+      aim: new FormControl(''),
+      cpc: new FormControl(''),
+      ctr: new FormControl(''),
+      romi: new FormControl(''),
+      nichaCategoryId: new FormControl(''),
+      userId: new FormControl(''),
+      indicatorEnd: this.indicatorEnd(),
+      steps: new FormArray([this.steps()]),
+      indicatorStart: this.indicatorEnd(),
+      creatives: new FormArray([this.creatives()]),
+      instagramCompanies: new FormArray([this.instagramCompanies()]),
+    });
   }
 
+  indicatorStart() {
+    return new FormGroup({
+      dailyBid: new FormControl('', Validators.required),
+      subscribers: new FormControl('', Validators.required),
+      dailyEarnings: new FormControl('', Validators.required),
+    });
+  }
+  indicatorEnd() {
+    return new FormGroup({
+      dailyBid: new FormControl('', Validators.required),
+      subscribers: new FormControl('', Validators.required),
+      dailyEarnings: new FormControl('', Validators.required),
+    });
+  }
+
+  steps() {
+    return new FormGroup({
+      title: new FormControl('', Validators.required),
+      number: new FormControl('', Validators.required),
+      description: new FormControl(null, Validators.required),
+      substeps: new FormArray([this.substeps()]),
+    });
+  }
+  substeps() {
+    return new FormGroup({
+      title: new FormControl('', Validators.required),
+      actions: new FormControl('', Validators.required),
+    });
+  }
+
+  creatives() {
+    return new FormGroup({
+      picture: new FormControl('', Validators.required),
+    });
+  }
+
+  instagramCompanies() {
+    return new FormGroup({
+      name: new FormControl('', Validators.required),
+      link: new FormControl('', Validators.required),
+    });
+  }
+
+  addsteps() {
+    const control = this.CreateCaseForm.get('steps') as FormArray;
+    control.push(this.steps());
+  }
+  addsubsteps() {
+    const control = this.CreateCaseForm.get('substeps') as FormArray;
+    control.push(this.substeps());
+  }
+  deletestep() {
+    const control = this.CreateCaseForm.get('steps') as FormArray;
+
+    if (control.length > 0) {
+      control.removeAt(control.length - 1);
+    }
+  }
+  deleteatstep(index: any) {
+    const control = this.CreateCaseForm.get('steps') as FormArray;
+
+    control.removeAt(index);
+  }
+  deletesubstep() {
+    const control = this.CreateCaseForm.get('substeps') as FormArray;
+
+    if (control.length > 0) {
+      control.removeAt(control.length - 1);
+    }
+  }
+  deleteatsubstep(index: any) {
+    const control = this.CreateCaseForm.get('substeps') as FormArray;
+
+    control.removeAt(index);
+  }
 }
