81ea91d422c86370e8958a5750f0b46203f8444b
judinilson <judinilson0000@gmail.com>
2021-09-15 15:49:13 +0400

    nish dropdown list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,40 @@
-import {MatFormFieldModule} from '@angular/material/form-field'
-import {MatIconModule} from '@angular/material/icon';
-import {AppRoutingModule} from './app-routing.module';
-import {MatSelectModule} from '@angular/material/select';
-import {AppComponent} from './app.component';
-import {AuthWelcomeComponent} from './ui/auth/auth-welcome/auth-welcome.component';
-import {AuthLoginComponent} from './ui/auth/auth-login/auth-login.component';
-import {AuthRegisterComponent} from './ui/auth/auth-register/auth-register.component';
-import {AuthComponent} from "./ui/auth/auth.component";
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { AppRoutingModule } from './app-routing.module';
+import { MatSelectModule } from '@angular/material/select';
+import { AppComponent } from './app.component';
+import { AuthWelcomeComponent } from './ui/auth/auth-welcome/auth-welcome.component';
+import { AuthLoginComponent } from './ui/auth/auth-login/auth-login.component';
+import { AuthRegisterComponent } from './ui/auth/auth-register/auth-register.component';
+import { AuthComponent } from './ui/auth/auth.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import {MatInputModule} from '@angular/material/input';
+import { MatInputModule } from '@angular/material/input';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PlatformComponent } from './ui/platform/platform.component';
 import { PlatformNavComponent } from './ui/platform/platform-nav/platform-nav.component';
-import {PlatformMainComponent} from "./ui/platform/platform-main/platform-main.component";
+import { PlatformMainComponent } from './ui/platform/platform-main/platform-main.component';
 import { NewsComponent } from './ui/news/news.component';
 import { NewsMainComponent } from './ui/news/news-main/news-main.component';
 import { AccountComponent } from './ui/account/account.component';
-import {AccountMainComponent} from "./ui/account/account-main/account-main.component";
+import { AccountMainComponent } from './ui/account/account-main/account-main.component';
 import { TemplateNicheComponent } from './ui/templates/template-niche/template-niche.component';
 import { CasesComponent } from './ui/cases/cases.component';
 import { CasesMainComponent } from './ui/cases/cases-main/cases-main.component';
 import { CasesCreateComponent } from './ui/cases/cases-create/cases-create.component';
-import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 
-import { NgModule }      from '@angular/core';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {HttpRequestInterceptor} from "./core/helpers/http.interceptor";
-import {ErrorInterceptor} from "./core/helpers/error.interceptor";
+import { HttpRequestInterceptor } from './core/helpers/http.interceptor';
+import { ErrorInterceptor } from './core/helpers/error.interceptor';
 import { AccountCasesComponent } from './ui/account/account-cases/account-cases.component';
 
-import {VgCoreModule} from '@videogular/ngx-videogular/core';
-import {VgControlsModule} from '@videogular/ngx-videogular/controls';
-import {VgOverlayPlayModule} from '@videogular/ngx-videogular/overlay-play';
-import {VgBufferingModule} from '@videogular/ngx-videogular/buffering';;
+import { VgCoreModule } from '@videogular/ngx-videogular/core';
+import { VgControlsModule } from '@videogular/ngx-videogular/controls';
+import { VgOverlayPlayModule } from '@videogular/ngx-videogular/overlay-play';
+import { VgBufferingModule } from '@videogular/ngx-videogular/buffering';
 
 @NgModule({
   declarations: [
@@ -55,7 +56,7 @@ import {VgBufferingModule} from '@videogular/ngx-videogular/buffering';;
     CasesComponent,
     CasesMainComponent,
     CasesCreateComponent,
-    AccountCasesComponent
+    AccountCasesComponent,
   ],
   imports: [
     VgCoreModule,
@@ -72,13 +73,17 @@ import {VgBufferingModule} from '@videogular/ngx-videogular/buffering';;
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    MatSelectModule
+    MatSelectModule,
+    NgSelectModule,
   ],
   providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: HttpRequestInterceptor, multi: true },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpRequestInterceptor,
+      multi: true,
+    },
     { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
